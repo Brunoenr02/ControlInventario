3b2b56df1960c3ee6ad8b60566156374c1787a0c
Bruno <brunoancco02@gmail.com>
2023-09-21 22:01:45 -0500

    Prueba

---
+++ inventario.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"BIENVENIDO AL SISTEMA DE CONTROL DE INVENTARIO"<<endl;
+    cout<<"Categorias Disponibles"<<endl;
+    cout<<"1. Productos de Limpieza"<<endl;
+    cout<<"2. Productos de Aseo Personal"<<endl;
+    cout<<"3. Bebidas"<<endl;
+    cout<<"4. "<<endl;
+    cout<<"5. "<<endl;
+    
+    system("PAUSE");
+    return 0;
+}
