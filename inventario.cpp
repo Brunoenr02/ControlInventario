8a23181b7ccff64a1177c618071fd09988335011
Brunoenr02 <143133204+Brunoenr02@users.noreply.github.com>
2023-11-10 21:19:47 -0500

    Inventario v2

--- inventario.cpp
+++ inventario.cpp
@@ -1,67 +1,87 @@
 //https://drive.google.com/drive/folders/1ZPjmxjUomEtev_RO5KCptVgomTzzrsRV?usp=drive_link//
 #include <iostream>
+#include <string>
 using namespace std;
+
+struct Producto
+{
+    int tipo;
+    string nombre;
+    int cantidad;
+    bool abastecer;
+};
+
+void leerProducto(Producto &);
+void mostrarProducto(Producto);
+
 int main()
 {
-    char cat;
-    string pl,pap,pb;
-    int cl,cap,cb;
+    Producto p;
+    Producto lista[100];
+    char cat,cen;
 
-    cout<<"----- BIENVENIDO AL SISTEMA DE CONTROL DE INVENTARIO -----"<<endl;
-    cout<<""<<endl;
-    cout<<"Categorias Disponibles"<<endl;
-    cout<<"1. Productos de Limpieza"<<endl;
-    cout<<"2. Productos de Aseo Personal"<<endl;
-    cout<<"3. Bebidas"<<endl;
-    cout<<""<<endl;
-    cout<<"Ingrese Categoria: "<<endl;
-    cin>>cat;
-
-    switch(cat)
+    do
+    {
+        system("cls");
+        cout<<"------ BIENVENIDO AL SISTEMA DE CONTROL DE INVENTARIO ------"<<endl;
+        cout<<"\n1. Agregar Producto"<<endl;
+        cout<<"2. Modificar Producto"<<endl;
+        cout<<"3. Mostrar todos los Productos"<<endl;
+        cout<<"4. Buscar por tipo de producto"<<endl;
+        cout<<"0. Salir"<<endl;
+        cout<<"\nIngrese Categoria: ";
+        cin>>cat;
+        switch(cat)
         {
-        case '1':
-            do
+            case '0':
+                cout<<"\nSe perderan todos datos. Seguro que desea salir? (S/N): "<<endl;
+                cin>>cen;
+                if(cen!='S' && cen!='s')
                 {
-                cout<<"Ingrese Producto de Aseo Personal (0 para salir)"<<endl;
-                cin>>pl;
-                if(pl!="0")
-                    {
-                    cout<<"Ingrese Cantidad"<<endl;
-                    cin>>cl;
-                    }
+                    cat=-1;
                 }
-            while(pl!="0");
-            break;
-        case '2':
-            do
-                {
-                cout<<"Ingrese Producto de Aseo Personal (0 para salir)"<<endl;
-                cin>>pap;
-                if(pap!="0")
-                    {
-                    cout<<"Ingrese Cantidad"<<endl;
-                    cin>>cap;
-                    }
-                }
-            while(pap!="0");
-            break;
-        case '3':
-            do
-                {
-                cout<<"Ingrese Bebidas (0 para salir)"<<endl;
-                cin>>pb;
-                if(pb!="0")
-                    {
-                    cout<<"Ingrese Cantidad"<<endl;
-                    cin>>cb;
-                    }
-                }
-            while(pb!="0");
-            break;
+                break;
+            case '1':
+                leerProducto(p);
+                break;
+            case '2':
+                cout<<"OPCION 2"<<endl;
+                break;  
+            case '3':
+                cout<<"OPCION 3"<<endl;
+                break;
+            case '4':
+                cout<<"OPCION 4"<<endl;
+                break;
+            default:
+            cout<<"Categoria no valida. Intente nuevamente"<<endl;
         }
-    
-    cout<<"Gracias"<<endl;
-    
-    system("PAUSE");
+        system("PAUSE");
+    }while(cat!='0');
+
+    cout<<"FIN DEL PROGRAMA"<<endl;
+    system("cls");
     return 0;
+}
+
+void leerProducto(Producto &p)
+{
+    char op;
+    do
+    {
+        system("cls");
+        cout<<"\t|01. Limpieza|02. Aseo Personal|03. Bebidas|04. Snacks|"<<endl;
+        cout<<"\nIngrese Tipo de Producto: ";
+        cin>>p.tipo;
+        cin.ignore();
+        cout<<"Ingrese Producto: ";
+        getline(cin, p.nombre);
+        cout<<"Ingrese Cantidad: ";
+        cin>>p.cantidad;
+        cout<<"Se registro: ";
+        cout<<p.tipo<<" "<<p.nombre<<" "<<p.cantidad<<endl;
+        cout<<"Es correcto? (S/N): ";
+        cin>>op;
+    }while(op!='s' && op!='S');
+    cout<<"REGISTRO EXITOSO"<<endl;
 }
